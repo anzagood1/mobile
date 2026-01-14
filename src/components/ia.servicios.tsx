4920698ab9da6a936a2ae1fa4e87a8e466ea113f
Angel Zambrano <anzagood@espol.edu.ec>
2026-01-14 04:03:24 +0000

    Correccion

--- src/components/ia.servicios.tsx
+++ src/components/ia.servicios.tsx
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from '@google/genai';
 import { CourseData, AISummaryResponse } from './ResumenDeClasesStructures';
 
-const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+const API_KEY = "AIzaSyD1zxeqPOqYXSxtfEC9QDyKjhX3m9jA3YM";
 
 const CACHE_PREFIX = 'aiSummaryCache:';
 
